0bb6cae83d54c72b72af9123e3f025ccb31d30a7
Yan Ritsner <yan.ritsner1984@gmail.com>
2018-03-05 21:36:26 +0200

    balances

--- src/pages/wallets/wallets.ts
+++ src/pages/wallets/wallets.ts
@@ -3,6 +3,10 @@ import { NavController } from 'ionic-angular';
 import { AddwalletPage } from '../addwallet/addwallet';
 import { Storage } from '@ionic/storage';
 import { SystemProvider } from '../../providers/system/system';
+import { ApiProvider } from './../../providers/api/api';
+import { WalletInfo } from './../../data/wallet-info';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-wallets',
@@ -11,26 +15,38 @@ import { SystemProvider } from '../../providers/system/system';
 export class WalletsPage {
 
   public wallet : string;
+  public walletReady : boolean = false;
+  public walletSubs : Subscription = null;
+
+  public error: string;
+  public errorVisible: boolean = false;
+
+  public confirmedBalance: number;
+  public unconfirmedBalance: number;
 
   constructor(public navCtrl: NavController,
               public storage: Storage,
-              public system: SystemProvider) {
+              public system: SystemProvider,
+              public api: ApiProvider) {
   }
 
   ionViewWillEnter(){
-    let env = this;
+    let model = this;
     this.storage.get('wallet')
     .then( function (data) {
       if(data){
-        env.wallet = data.name;
-        env.system.setWalletName(env.wallet);
-      }
-      else{
-        env.wallet = "Please add or create a wallet."
+        model.wallet = data.name;
+        model.system.setWalletName(model.wallet);
+        model.walletReady = true;
+        model.loadWallet();
       }
     }, function (error) {
       console.log(error);
     });
+  }
+
+  ionViewWillUnload(){
+      this.unloadWallet();
   }
 
   send(){
@@ -49,5 +65,57 @@ export class WalletsPage {
     alert("createWallet")
   }
 
+  loadWallet()
+  {
+    this.unloadWallet();
 
+    let walletLoad = new WalletInfo(this.wallet);
+
+    let walletObs = this.api.getWalletBalance(walletLoad);
+    this.walletSubs = walletObs.subscribe(
+      response => {
+        if (response.status >= 200 && response.status < 400) {
+
+          let balanceResponse = response.json();
+          this.confirmedBalance = balanceResponse.balances[0].amountConfirmed;
+          this.unconfirmedBalance = balanceResponse.balances[0].amountUnconfirmed;
+
+          this.errorVisible = false;
+        }
+      },
+      error => {
+        
+        if (error.status === 0) {
+          this.error = "Could not login wallet info"
+        
+        } else if (error.status >= 400) {
+          if (!error.json().errors[0]) {
+            this.error = error;
+          }
+          else {
+            this.error = error.json().errors[0].message;
+          }
+        }
+
+        this.errorVisible = true;
+      }
+    );
+  }
+
+  unloadWallet()
+  {
+    if(this.walletSubs == null) return;
+    this.walletSubs.unsubscribe();
+  }
+
+  balance(value)
+  {
+    if(value == null) return "";
+    let val = value / 100000000;
+    var factor = Math.pow(10, 8);
+    var formatted =  (Math.round(val * factor) / factor).toLocaleString();
+    if(formatted.indexOf(".") < 0)
+    formatted += ".0";
+    return formatted;
+  }
 }
