4c9ce2e68d968dd241f9d74b4a4e8adcd30d0f19
Yan Ritsner <yan.ritsner1984@gmail.com>
2018-03-10 22:22:50 +0200

    send

--- src/pages/send/send.ts
+++ src/pages/send/send.ts
@@ -7,11 +7,21 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SendPage {
 
+  amount: number;
+  address: string;
+  fee: string ="medium";
+  password:string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SendPage');
+  }
+
+  doSend()
+  {
+
   }
 
 }
