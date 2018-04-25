4467bc1f5aa3989f8be9a388879e987dd9135e73
Yan Ritsner <yan.ritsner1984@gmail.com>
2018-04-25 23:06:14 +0300

    contact name

--- src/data/message-data.ts
+++ src/data/message-data.ts
@@ -5,11 +5,13 @@ export class MessageData {
       this.date = date;
       this.out = out;
       this.day = this.getDay();
+      this.main = this.getMin();
     }
   
     public message: string;
     public date: Date;
     public day: number;
+    public min: number;
     public out: boolean;
 
     getDay()
@@ -19,5 +21,11 @@ export class MessageData {
       return d.valueOf();
     }
 
+    getMin()
+    {
+      var d = new Date(this.date);
+      d.setSeconds(0,0);
+      return d.valueOf();
+    }
   }
   
