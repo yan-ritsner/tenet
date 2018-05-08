02f90ff5811b1150084d9915c5ce3ebcc73d5076
Yan Ritsner <yan.ritsner1984@gmail.com>
2018-05-08 20:14:37 +0300

    aws api

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -23,7 +23,7 @@ export class ApiProvider {
 
   private headers = new Headers({'Content-Type': 'application/json'});
   private pollingInterval = 1000;
-  private currentApiUrl = 'http://192.168.10.115:37222/api';
+  private currentApiUrl = 'http://52.16.204.42:37222/api';
 
   constructor(public http: Http) {
   
